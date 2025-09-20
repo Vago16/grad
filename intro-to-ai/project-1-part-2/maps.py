f2e31e342626c3de61dfa90f00d5ef4212290e52
Vagelis Petropoulos <vagelispetropoulos@yahoo.com>
2025-09-19 23:48:18 -0400

    added maps 2 and 3

--- intro-to-ai/project-1-part-2/maps.py
+++ intro-to-ai/project-1-part-2/maps.py
@@ -41,6 +41,7 @@ def print_map(map, num=None):     #takes map as first arg, and then map number(n
    # pass
 
 #initiliaze dictionary for costs of traversing different terrains
+#this shows additional costs of traversing terrain
 terrain_costs = {
     Terrain.base: 0,      #1 base tile always in 0,0
     Terrain.empty: 0,    #for simplicity, 5 empty tiles  
@@ -58,6 +59,24 @@ map_1 = [
     [5, 4, 2, 3, 2]
 ]
 
+#2-D array representation of the first map
+map_2 = [
+    [0, 1, 4, 5, 5],
+    [1, 1, 4, 4, 3],
+    [4, 4, 1, 2, 3],
+    [5, 3, 2, 1, 2],
+    [5, 3, 3, 2, 2]
+]
+
+#simple straightforward map mainly for testing to make sure search alg works 
+map_3 = [
+    [0, 1, 1, 1, 1],
+    [5, 5, 5, 5, 1],
+    [4, 4, 4, 4, 2],
+    [4, 2, 2, 3, 2],
+    [3, 3, 3, 3, 3]
+]
+
 #list of resources to collect for map_1
 resource_list_1 = {
     "stone": [(0,2), (2,1), (3,4)],  #list of where the 3 stones are in the map
@@ -65,4 +84,20 @@ resource_list_1 = {
     "crystal": [(1,2)]   #list of where the 1 crystal is in the map
 }
 
+#list of resources to collect for map_2
+resource_list_2 = {
+    "stone": [(1,1), (2,2), (3,3)],  #list of where the 3 stones are in the map
+    "iron": [(2,4), (4,2)],    #list of where the 2 irons are in the map
+    "crystal": [(4,0)]   #list of where the 1 crystal is in the map
+}
+
+#list of resources to collect for map_3
+resource_list_3 = {
+    "stone": [(0,1), (0,2), (0,3)],  #list of where the 3 stones are in the map
+    "iron": [(0,4), (1,4)],    #list of where the 2 irons are in the map
+    "crystal": [(2,4)]   #list of where the 1 crystal is in the map
+}
+
 print_map(map_1, 1)
+print_map(map_2, 2)
+print_map(map_3, 3)
