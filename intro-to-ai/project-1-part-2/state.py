9daeb0c4420be8c6e8b27283231b8f742931f749
Vagelis Petropoulos <vagelispetropoulos@yahoo.com>
2025-09-20 17:02:08 -0400

    initialized state class and init

--- intro-to-ai/project-1-part-2/state.py
+++ intro-to-ai/project-1-part-2/state.py
@@ -1,1 +1,13 @@
 #represents state of the map
+
+#define class for state of the map
+class State:
+    def __init__(self, pos, backpack, finished):
+        '''
+        pos- position on the map in row and column of the agent
+        backpack- tuple representing leather backpack that can only hold up to two resources at a time at most
+        finished- a dict with the resources that have been taken successfully to the base(0,0) coordinate
+        '''
+        self.pos = pos
+        self.backpack = tuple(backpack)
+        self.finished = dict(finished) 
