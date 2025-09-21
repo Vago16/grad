fa81727fcb55e019f68fdc62d778289184b16878
Vagelis Petropoulos <vagelispetropoulos@yahoo.com>
2025-09-21 05:10:21 -0400

    added the capability to change resource tiles to regular terrain tiles once the resource has been picked up

--- intro-to-ai/project-1-part-2/state.py
+++ intro-to-ai/project-1-part-2/state.py
@@ -4,52 +4,102 @@ import maps
 
 #define class for state of the map, each object of state class represents a node in search
 class State:
-    def __init__(self, pos, backpack, finished):
+    def __init__(self, pos, backpack, finished, resources):
         '''
         pos- tuple showing position on the map in row and column of the agent
         backpack- tuple representing leather backpack that can only hold up to two resources at a time at most
         finished- a dict with the resources that have been taken successfully to the base(0,0) coordinate
+        resources- a dictionary with a list of copies of each resource
         '''
         self.pos = pos
         self.backpack = tuple(backpack)
         self.finished = dict(finished) 
+
+        self.resources = {}
+        for resource, coordinates in resources.items(): #iterate through each of the items(resources) and go through each tile on the map
+            self.resources[resource] = list(coordinates)
     
     def __eq__(self, other):        #checks state against another specified state to see if they are equal(position, contents of backpack, and however many resources have been taken to base)
         return(
             self.pos == other.pos and
             self.backpack == other.backpack and
-            self.finished == other.finished
+            self.finished == other.finished and
+            self.resources == other.resources
         )
     
     def __hash__(self):     #hash the state(making tuple out of self.finished lets it be hashable)
-        return hash((self.pos, self.backpack, tuple(self.finished.items())))
+        resource_items = []
+        for resource, coordinates in self.resources.items(): #iterate through each of the items(resources) and go through each tile on the map
+            resource_items.append((resource, tuple(coordinates)))       #convert each list to tuples before appending to allow hashing
+        resources_tupled = tuple(resource_items)        #convert overall list to tuple
+
+        return hash((self.pos, self.backpack, tuple(self.finished.items()), resources_tupled))
 
     def __repr__(self):     #print out state
-        return f"Position: {self.pos}, Contents in Backpack: {self.backpack}, Resources delivered: {self.finished}"
+        return f"Position: {self.pos}, Contents in Backpack: {self.backpack}, Resources delivered: {self.finished}, Resources left: {self.resources}"
 
-#move function to move the agent to adjacent tiles
-def move_agent():
+#move function to handle moving the agent to adjacent tiles
+def move_agent(state, new_pos, given_map, terrain_costs):
     '''
     Move the agent to a new position on the map and pick up/drop off resources
     '''
-    pass
+    #copy backpack and its contents(if any) to a list, and copy delivered resources to a dict
+    new_pack = list(state.backpack) 
+    new_finished = dict(state.finished)
+    #make a dict copy of resouces that will not modify original state
+    new_resources = {}
+    for resource, coordinates in state.resources.items(): #iterate through each of the items(resources) and go through each tile on the map
+        new_resources[resource] = list(coordinates)
+
+    #if the tile has resource and backpack has space, pick up resource
+    for resource, coordinate in new_resources.items(): #iterate through coordinates and resource items in the list
+        if new_pos in coordinate and len(new_pack) < 2:     #if there is room in the backpack(less than 2 items currently)
+            new_pack.append(resource)
+            coordinate.remove(new_pos) #remove resource from map
+
+    #if the tile is the base tile
+    if new_pos == (0,0) and new_pack:   #(0,0) is always base and makes sure there is a new_pack variable that is True
+        for resource in new_pack:
+            new_finished[resource] += 1     #add resources to count 
+        new_pack = []       #empty backpack completely
+
+    #return new state(node)
+    return State(
+        pos=new_pos,
+        backpack=new_pack,
+        finished=new_finished,
+        resources=new_resources
+    )
 
 #get_neighbor function
 #I got some help from https://stackoverflow.com/questions/77274736/how-to-find-neighbors-in-a-grid for figuring it out
-def get_neighbor(state, map, resource_list, terrain_costs):
+def get_neighbor(state, given_map, terrain_costs):
     '''
     Return a list of all adjacent(neighbor) states from current state 
     and their movement costs.
     '''
     neighbors = []     #list to be appended and returned at end of function
-    rows, columns = len(map), len(map[0])   #gets the dimensions of the map
+    rows, columns = len(given_map), len(given_map[0])   #gets the dimensions of the map
     curr_row, curr_col = state.pos
 
     directions = [(-1,0), (1,0), (0,-1), (0,1)] #all the possible directions that can be moved to in a 2-D array
 
     for dir_row, dir_col in directions:   #for each direction
-        neighor_row, neighbor_col = curr_row + dir_row, curr_col + dir_col
+        neighbor_row = curr_row + dir_row   #get neighbor coordinates
+        neighbor_col = curr_col + dir_col  
 
+        if 0 <= neighbor_row < rows and 0 <= neighbor_col < columns:    #makes sure that the agent cannot move off the map in error
+            new_pos = (neighbor_row, neighbor_col)  #stores neighboring states positions
+            neighbor_state = move_agent(state, new_pos, given_map, terrain_costs)     #call move_agent function to move agent
+
+            #get the cost for moved onto tile/terrain
+            terrain = given_map[neighbor_row][neighbor_col]
+            movement_cost = terrain_costs[terrain] + 1  #get the movement cost for the specific terrain(and add 1 as it always costs 1 to move)
+
+            #append to list for the state and movement cost
+            neighbors.append((neighbor_state, movement_cost))
+
+    return neighbors
 
 
 
@@ -58,19 +108,22 @@ def get_neighbor(state, map, resource_list, terrain_costs):
 state1 = State(
     pos=(0, 0),
     backpack=(),
-    finished={"Stone": 0, "Iron": 0, "Crystal": 0}
+    finished={"Stone": 0, "Iron": 0, "Crystal": 0},
+    resources=maps.resource_list_3
 )
 
 state2 = State(
     pos=(0, 0),
     backpack=(),
-    finished={"Stone": 0, "Iron": 0, "Crystal": 0}
+    finished={"Stone": 0, "Iron": 0, "Crystal": 0},
+    resources=maps.resource_list_3
 )
 
 state3 = State(
     pos=(0, 3),
     backpack=(),
-    finished={"Stone": 0, "Iron": 1, "Crystal": 0}
+    finished={"Stone": 0, "Iron": 1, "Crystal": 0},
+    resources=maps.resource_list_3
 )
 
 #testing equality
@@ -92,3 +145,12 @@ print(len(visited2))
 print(state1)
 print(state2)
 print(state3)
+print() #newline for readability
+
+#Testing get_neighbor function
+neighbors = get_neighbor(state1, maps.map_3, maps.terrain_costs)
+
+for neighbor, cost in neighbors:
+    print(f"Move to {neighbor.pos} with cost {cost}")
+    print(f"  Backpack: {neighbor.backpack}, Delivered: {neighbor.finished}")
+    print()
